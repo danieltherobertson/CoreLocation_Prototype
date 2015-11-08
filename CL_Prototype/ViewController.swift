fd7171082a7e43d859f3932ad42239acb607aa04
Daniel Robertson <danieltherobertson@gmail.com>
2015-11-08 12:12:56 +0000

    Initial Commit

---
+++ CL_Prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CL_Prototype
+//
+//  Created by Daniel Robertson on 08/11/2015.
+//  Copyright © 2015 Daniel Robertson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
